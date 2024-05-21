ca65515efe2131b141ec082595fe1855752b783d
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-21 12:30:12 +0000

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,9 +4,7 @@ gather info from api using requests module
 """
 
 import json
-import os
 import requests
-import sys
 
 
 if __name__ == "__main__":
@@ -14,34 +12,29 @@ if __name__ == "__main__":
     """
     gathering info from resful API
     """
-    user_id = sys.argv[1]
 
-    res_todo = requests.get(
-            "https://jsonplaceholder.typicode.com/todos?userId={}"
-            .format(user_id))
     res_user = requests.get(
-            "https://jsonplaceholder.typicode.com/users?id={}"
-            .format(user_id))
+            "https://jsonplaceholder.typicode.com/users")
 
-    user_name = res_user.json()[0]["username"]
-    user_all_task = res_todo.json()
+    user_all = res_user.json()
+    user_name = user_all[0]["username"]
 
-    arr = []
-    for x in user_all_task:
-        new_dic = {}
-        new_dic["username"] = user_name
-        new_dic["task"] = x["title"]
-        new_dic["completed"] = x["completed"]
-        arr.append(new_dic)
+    all_tog = {}
+    for x in user_all:
+        res_todo = requests.get(
+                "https://jsonplaceholder.typicode.com/todos?userId={}"
+                .format(x["id"]))
+        user_todo = res_todo.json()
 
-    if not os.path.exists("todo_all_employees.json"):
-        with open("todo_all_employees.json", "w") as f:
-            json.dump({}, f)
+        arr = []
+        for y in user_todo:
+            dic = {}
+            dic["username"] = user_name
+            dic["task"] = y["title"]
+            dic["completed"] = y["completed"]
+            arr.append(dic)
 
-    with open("todo_all_employees.json", "r") as f:
-        all_tog = json.load(f)
+        all_tog[x["id"]] = arr
 
-    all_tog[f"{user_all_task[0]['userId']}"] = arr
-
-    with open("todo_all_employees.json", "w") as f:
+    with open("todo_all_employees.json", "w+") as f:
         json.dump(all_tog, f, indent=4)
