b091da05ab6e363394eb6e962d1cd67996f82983
DentistOmarAshraf <omar_ashraf@live.com>
2024-05-21 02:00:13 +0000

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+gather info from api using requests module
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    """
+    gathering info from resful API
+    """
+    user_id = sys.argv[1]
+
+    res_todo = requests.get(
+            "https://jsonplaceholder.typicode.com/todos?userId={}"
+            .format(user_id))
+    res_user = requests.get(
+            "https://jsonplaceholder.typicode.com/users?id={}"
+            .format(user_id))
+
+    user_name = res_user.json()[0]["name"]
+    user_all_task = res_todo.json()
+
+    completed = []
+    for x in user_all_task:
+        if x["completed"]:
+            completed.append(x["title"])
+
+    print("Employee {} is done with tasks ({}/{})"
+          .format(user_name, len(completed), len(user_all_task)))
+
+    for x in completed:
+        print("\t{}".format(x))
