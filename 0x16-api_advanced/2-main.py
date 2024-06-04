9bff81e6d234fd14abd225c3fbb43aded96b122a
DentistOmarAshraf <omar_ashraf@live.com>
2024-06-04 20:54:06 +0000

    2-recurse.py

---
+++ 0x16-api_advanced/2-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+2-main
+"""
+import sys
+
+if __name__ == '__main__':
+    recurse = __import__('2-recurse').recurse
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+            print(len(set(result)))
+            for x in result:
+                print(x)
+        else:
+            print("HERE None")
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Recursive Fetch from api
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+
+    if len(hot_list) > 0 and hot_list[-1]["after"] is None:
+        print("hot_list after {}".format(hot_list[-1]["after"]))
+        to_ret = []
+        for item in hot_list:
+            to_ret = to_ret + item["children"]
+        return to_ret
+
+    if len(hot_list) > 0:
+        res = requests.get(
+                    "https://www.reddit.com/r/{}/hot/.json?limit=10&after={}"
+                    .format(subreddit, hot_list[-1]["after"]),
+                    allow_redirects=False)
+
+    else:
+        res = requests.get("https://www.reddit.com/r/{}/hot/.json?limit=10"
+                           .format(subreddit),
+                           allow_redirects=False)
+    dt = {}
+    if res.status_code == 200:
+        dt["after"] = res.json()["data"]["after"]
+        dt["children"] = []
+        for title in res.json()["data"]["children"]:
+            dt["children"].append(title["data"]["title"])
+        hot_list.append(dt)
+
+    return recurse(subreddit, hot_list)
