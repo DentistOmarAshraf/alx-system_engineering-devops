120f33e4daf70f396ccdbbe9d4a97d40358b4a31
DentistOmarAshraf <omar_ashraf@live.com>
2024-06-04 15:04:29 +0000

    ---

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,21 @@
 """
 Fetch ReditAPI
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Funciton to fetch reddit API subscribers"""
+
+
     res = requests.get("https://www.reddit.com/r/{}/about/.json"
                        .format(subreddit),
                        allow_redirects=False,
                        headers={"User-Agent": "My-Agent"})
+
     if res.status_code == 200:
         return int(res.json()["data"]["subscribers"])
+
     else:
         return 0
